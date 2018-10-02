a60e82fc03d2a77dee4d91af0024dee51a3a4d6d
autumdowney <ardowney@mix.wvu.edu>
2018-10-02 15:10:00 -0400

    a test loop only. made for 1 site

---
+++ test.sh
@@ -0,0 +1,6 @@
+for gauge in "02109500"
+do
+wget -O "$gauge".txt "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&format=rdb&site_no=$gauge&period=&begin_date=2018-09-25&end_date=2018-10-01"
+sort -r "$gauge".txt | head -n 2 | tail -n 1 >> most_recent_discharge.txt
+done
+#test loop
