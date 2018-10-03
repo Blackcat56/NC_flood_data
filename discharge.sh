f32c3c23677ffdf37e8fff7a21fb3f18a395d078
Amanda Adams <aeadams@mix.wvu.edu>
2018-10-03 12:10:20 -0400

    I fixed what i broke. I made it so that there was >> instead of >. So that it doesnt override for the final project.The > would be good for practicing the loop so that we can see if the loop works or not. A problem we had on tuesday.

--- discharge.sh
+++ discharge.sh
@@ -3,5 +3,5 @@ for gauge in "02109500" "02134500" "02091814" "02105769"
 do
 wget -O "$gauge".txt "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&format=rdb&site_no=$gauge&period=&begin_date=2018-09-25&end_date=2018-10-01"
 #This line sorts the data most recent data at the top and show just the first line of data.
-sort -r "$gauge".txt | head -n 2 | tail -n 1 > most_recent_discharge.txt
+sort -r "$gauge".txt | head -n 2 | tail -n 1 >> most_recent_discharge.txt
 done
