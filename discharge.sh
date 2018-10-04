8b2a3c04791667a33c0f8cc2801f1b017f1c8823
Jill Riddell <jlriddell@mix.wvu.edu>
2018-10-04 15:01:41 -0400

    Updated discharge loop to include variable to pull discharge data from most recent date. Note the syntas of hte variable  doesn't have quotes around it in the for....line

--- discharge.sh
+++ discharge.sh
@@ -1,10 +1,19 @@
-#This loop is used for getting certain river gages most recent duscharge.txt.
-for gauge in "02109500" "02134500" "02091814" "02105769"
+#Download discharge data for gauging stations of interests in NC from USGS website
+#Usage bash discharge.sh date +%Y-%m-%d 
+
+for gauge in "02109500" "02134500" "02091814" "02105769" $1
 do
-wget -O "$gauge".txt "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&format=rdb&site_no=$gauge&period=&begin_date=2018-09-25&end_date=2018-10-01"
-sed -n '17p' "$gauge".txt >> flow_out.txt
-tail -n 1 "$gauge".txt >> flow_out.txt
+	wget -O "$gauge".txt "https://waterdata.usgs.gov/nc/nwis/uv?cb_00060=on&format=rdb&site_no=$gauge&period=&begin_date=2018-09-25&end_date=$1"
+	sed -n '17p' "$gauge".txt >> flow.txt
+        tail -n 1 "$gauge".txt >> flow.txt	
 done
 
-#This code line combines the two lines from flow output into one line for the new text file, most_recent_discharge. 
-paste - - < flow_out.txt > most_recent_discharge.txt
+#This loop is used for getting certain river gauges most recent discharge on today's date. 
+#Note that gauge number could be made a variable as well if additional gauges became of interest.
+
+paste - - < flow.txt > most_recent_discharge.txt
+#This code line combines the two lines from flow output into one line for the new text file, most_recent_discharge.
+
+rm flow.txt
+#Remove unnecessary files.
+#Note that discharge is in cfs
